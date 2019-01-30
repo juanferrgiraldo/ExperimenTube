11de237cb2feb95d202f3570f88a895f85b0f40c
Juan Fernando <juanferrgiraldo@gmail.com>
2019-01-30 02:14:03 -0500

    Final arrangements over mobile layout

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { Constants } from '../constants';
+
 @Injectable()
 export class SearchService {
   constructor (private httpClient: HttpClient) { }
 
-  url = 'https://www.googleapis.com/youtube/v3/search';
-  apiKey = 'AIzaSyC7XqLu8D3YNelLD7YXsHYZXsdugeHcf6M';
-
   searchVideo(video) {
-    const params = new HttpParams().set('part', 'snippet').set('maxResults', '6').set('q', video).set('key', this.apiKey);
-    return this.httpClient.get<any>(this.url, {params}).pipe(
+    const params = new HttpParams().set('part', 'snippet').set('maxResults', '6').set('q', video).set('key', Constants.APIK);
+    return this.httpClient.get<any>(Constants.URL, {params}).pipe(
       map( results => {
         return results.items;
       })
@@ -19,8 +18,8 @@ export class SearchService {
   }
 
   trendingVideos() {
-    const params = new HttpParams().set('part', 'snippet').set('chart', 'mostPopular').set('regionCode', 'co').set('key', this.apiKey);
-    return this.httpClient.get<any>(this.url, {params}).pipe(
+    const params = new HttpParams().set('part', 'snippet').set('chart', 'mostPopular').set('regionCode', 'co').set('key', Constants.APIK);
+    return this.httpClient.get<any>(Constants.URL, {params}).pipe(
       map( results => {
         return results.items;
       })
